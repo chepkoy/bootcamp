7bf5b1452f852817e08dbb9577eda8b83354f731
chepkoy <allankiplangat22@gmail.com>
2018-02-20 01:16:23 +0300

    The Adder

--- flask_basics/simple_app.py
+++ flask_basics/simple_app.py
@@ -9,6 +9,9 @@ def index(name="Treehouse"):
 
 
 @app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
     return '{} + {} = {}'.format(num1, num2, num1+num2)
 app.run(debug=True, port=5000)
